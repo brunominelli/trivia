e7598a41ef61486873d325c6e18700548e87b2cf
Bryan Fernandes <bryan.efernandes@gmail.com>
2022-04-08 09:07:19 -0300

    Requisito 18 e Fix: Reset de Assertions e receive Token após 2º login

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,10 +1,37 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { readFeedback } from '../services/localStorage';
 
 class Ranking extends Component {
   redirectLogin = () => {
     const { history } = this.props;
     history.push('/');
+  }
+
+  handleRanking = () => {
+    /* Referência sort array by biggest number: https://www.codegrepper.com/code-examples/javascript/javascript+sort+array+by+biggest+number */
+    const list = readFeedback()
+      .sort((previous, current) => current.score - previous.score);
+
+    return (
+      <>
+        {list.map((feedback, index) => (
+          <div key={ index }>
+            <img src={ feedback.picture } alt="Gravatar" />
+            <div
+              data-testid={ `player-name-${index}` }
+            >
+              {feedback.name}
+            </div>
+            <div
+              data-testid={ `player-score-${index}` }
+            >
+              {feedback.score}
+            </div>
+          </div>
+        ))}
+      </>
+    );
   }
 
   render() {
@@ -15,6 +42,12 @@ class Ranking extends Component {
         >
           Ranking
         </h1>
+        <div>
+          { readFeedback().length === 0
+            ? <div>Ranking vazio</div>
+            : this.handleRanking()}
+
+        </div>
         <button
           data-testid="btn-go-home"
           type="button"
