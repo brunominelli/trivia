61ff44c88665e2124f71c262aca75717ece3db83
Bryan Fernandes <bryan.efernandes@gmail.com>
2022-04-08 07:50:05 -0300

    Requisito 17

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,6 +1,12 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Ranking extends Component {
+  redirectLogin = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <div>
@@ -9,9 +15,20 @@ class Ranking extends Component {
         >
           Ranking
         </h1>
+        <button
+          data-testid="btn-go-home"
+          type="button"
+          onClick={ this.redirectLogin }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.func,
+}.isRequired;
 
 export default Ranking;
