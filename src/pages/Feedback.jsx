a92d8592d7fbffe2f44feae862a293bf027589b3
Bryan Fernandes <bryan.efernandes@gmail.com>
2022-04-08 07:07:20 -0300

    Requisito 15

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,3 +1,4 @@
+import md5 from 'crypto-js/md5';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
@@ -8,14 +9,22 @@ class Feedback extends Component {
   componentDidMount() {
     const { name, gravatarEmail, assertionsProps, scoreProps } = this.props;
 
+    const playerImage = md5(gravatarEmail).toString();
+    const picture = `https://www.gravatar.com/avatar/${playerImage}`;
+
     if (!JSON.parse(localStorage.getItem('rankList'))) {
       localStorage.setItem('rankList', JSON.stringify([]));
     }
 
     const info = {
-      name, score: scoreProps, assertions: assertionsProps, picture: gravatarEmail };
+      name, score: scoreProps, assertions: assertionsProps, picture };
     const storageRanking = readFeedback();
     saveFeedback([...storageRanking, info]);
+  }
+
+  redirect = () => {
+    const { history } = this.props;
+    history.push('/');
   }
 
   render() {
@@ -41,6 +50,13 @@ class Feedback extends Component {
         >
           {scoreProps}
         </div>
+        <button
+          data-testid="btn-play-again"
+          type="button"
+          onClick={ this.redirect }
+        >
+          Play Again
+        </button>
       </>
     );
   }
